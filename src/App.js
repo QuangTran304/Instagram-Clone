4035c4f24a338a554a668b6c5d6e3d60f4d4f812
Quang <nhatquang304@gmail.com>
2021-02-13 00:19:39 -0500

    create and style basic Nav bar #28

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './Instagrill.svg';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import ExploreOutlinedIcon from '@material-ui/icons/ExploreOutlined';
+import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+import FaceRoundedIcon from '@material-ui/icons/FaceRounded';
+
 
 function App() {
   return (
     <div className="app">
 
-      <div className="navBar">
-        <li><img src={logo} className="App-logo" alt="logo" /></li>
+
+      <div className="nav-bar">
+        <div className="nav-container">
+        <div>
+          <img src={logo} className="app-logo" alt="logo" />
+        </div>
+
+        <div className="search-box">
+          <input type="text" name="search" placeholder="Search..."/>
+        </div>
+
+        <div className="menu">
+          <HomeRoundedIcon className="icon-item" />
+          <ExploreOutlinedIcon className="icon-item" />
+          <FavoriteBorderRoundedIcon className="icon-item" />
+          <FaceRoundedIcon className="icon-item" />
+        </div>
+        </div>
       </div>
 
       <div className="post">
@@ -13,11 +34,9 @@ function App() {
           <h1>Post head: Username & profile picture</h1>
         </div>
 
-
         <div className="post-image">
           <img src="https://images.pexels.com/photos/4041098/pexels-photo-4041098.jpeg" alt=""/>
         </div>
-
 
         <div className="post-description">
           <p>
@@ -25,6 +44,7 @@ function App() {
           </p>
         </div>
       </div>
+
 
     </div>
   );
