960380ba4074907d3e384dad1255fd11e12a3ee8
Quang <nhatquang304@gmail.com>
2021-02-14 20:56:24 -0500

    replace svg logo to png logo to avoid display error

--- src/Post.js
+++ src/Post.js
@@ -9,27 +9,27 @@ const Post = () => {
   return (
     data.posts.map( (post) => (
       <div className="post">
-      <div className="post-header">
-        <Avatar
-          className="post-avatar"
-          alt={post.username}
-          src="/static/images/avatar/1.jpg"
-        ></Avatar>
-        <h3>{post.username}</h3>
-      </div>
-      <img className="post-image" src={post.image} alt="" />
-      <div className="post-icons">
-        <FavoriteBorder className="post-heart" />
-        <ChatBubbleOutlineOutlinedIcon />
-      </div>
-      <p className="post-like-number"> Liked by {post.likes} people</p>
+        <div className="post-header">
+          <Avatar
+            className="post-avatar"
+            alt={post.username}
+            src="/static/images/avatar/1.jpg"
+          ></Avatar>
+          <h3>{post.username}</h3>
+        </div>
+        <img className="post-image" src={post.image} alt="" />
+        <div className="post-icons">
+          <FavoriteBorder className="post-heart" />
+          <ChatBubbleOutlineOutlinedIcon />
+        </div>
+        <p className="post-like-number"> Liked by {post.likes} people</p>
 
-      <h4 className="post-text">
-        <strong>{post.username}</strong> {post.description}
-      </h4>
+        <h4 className="post-text">
+          <strong>{post.username}</strong> {post.description}
+        </h4>
 
-      <h3 className="post-comment">View all comments.</h3>
-    </div>
+        <h3 className="post-comment">View all comments.</h3>
+      </div>
     )) 
   );
 };
