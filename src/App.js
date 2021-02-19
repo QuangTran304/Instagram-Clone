756206a699f1b144fe2bb45f370da8323438315b
naregschoolwork <naregschoolwork@gmail.com>
2021-02-18 21:20:01 -0500

    #28 Turned the navbar into a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import logo from "./Instagrill.png";
-import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
-import ExploreOutlinedIcon from "@material-ui/icons/ExploreOutlined";
-import FavoriteBorderRoundedIcon from "@material-ui/icons/FavoriteBorderRounded";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import CreatePost from "./CreatePost";
 import Post from "./Post";
+import Navbar from "./Navbar";
 
 
 function App() {
@@ -12,27 +8,7 @@ function App() {
   return (
     <div className="app">
 
-      {/* We should make this Nav bar a Component */}
-      <div className="nav-bar">
-        <div className="nav-container">
-          <div>
-            <img src={logo} className="app-logo" alt="logo" />
-          </div>
-
-          <div className="search-box">
-            <input type="text" name="search" placeholder="Search..." />
-          </div>
-
-          <div className="menu">
-            <HomeRoundedIcon className="icon-item" />
-            <ExploreOutlinedIcon className="icon-item" />
-            <FavoriteBorderRoundedIcon className="icon-item" />
-            <AccountCircleIcon className="icon-item" />
-          </div>
-        </div>
-      </div>
-      {/* We should make this Nav bar a Component */}
-
+      <Navbar />
 
       <CreatePost />
 
---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+
+     import logo from "./Instagrill.png";
+     import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
+     import ExploreOutlinedIcon from "@material-ui/icons/ExploreOutlined";
+     import FavoriteBorderRoundedIcon from "@material-ui/icons/FavoriteBorderRounded";
+     import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+     const Navbar = () => {
+         return(
+             <div className="nav-bar">
+      <div className="nav-container">
+        <div>
+          <img src={logo} className="app-logo" alt="logo" />
+        </div>
+
+        <div className="search-box">
+          <input type="text" name="search" placeholder="Search..." />
+        </div>
+
+        <div className="menu">
+          <HomeRoundedIcon className="icon-item" />
+          <ExploreOutlinedIcon className="icon-item" />
+          <FavoriteBorderRoundedIcon className="icon-item" />
+          <AccountCircleIcon className="icon-item" />
+        </div>
+      </div>
+    </div>
+         )
+     }
+
+     export default Navbar;
+     
