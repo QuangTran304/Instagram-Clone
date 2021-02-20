8187d5494c3b254f5e02df7bacd13f76ab13587c
Mélina Deneuve <na-li-me@hotmail.fr>
2021-02-19 21:54:40 -0500

    Styling changes #36 post upload form

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,26 +1,26 @@
 import React from "react";
 import "./index.css";
-import {useState} from "react";
+import { useState } from "react";
 
 
 const CreatePost = () => {
- 
 
+    var Nickname = "pseudo";
     const [description, setDescription] = useState("");
     return (
         <div className="create">
-        <h2>Create a new Post !</h2>
+        <h4 class="title"> Create a new Post ! </h4>
         <form>
-            <label>Write your description:</label><br />
-		    <input type="text" name="description" onfocus="this.value=''" value= {description} required onChange={(e) => setDescription(e.target.value)}/> <br />
-        
-            <label for="file">Select the image you want to share:</label>
-            <input type="file" id="image" name="create-image" accept="image/png, image/jpeg, image/jpg" required />
-            <br /><br />
-            <button type="submit">Post</button>
+        <label> Write your description: </label><br />
+        <textarea class="caca" type="text" name="description" onfocus="this.value=''" value = { description } required onChange = { (e) => setDescription(e.target.value) }/>
+        <br/>
+        <label for = "file"> Select the image you want to share: </label>
+        <input type="file" id = "image" name="create-image" accept="image/png, image/jpeg, image/jpg" required />
+        <br /> < br />
+        <button type="submit" > Post </button> 
         </form>
-        <p><br/>{description}</p>
-        <p></p>
+        <p><br/> { description } </p>
+        <p> </p>
         </div>
     );
 };
