be2fb611b7d90c6cf2d5f2158d1e6f1b30aeae93
Anonna Chowdhury <anonna2010@hotmail.com>
2021-02-21 19:42:37 -0500

    Update CreatePost.js to send data to firease

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -2,13 +2,52 @@ import React from "react";
 import "./index.css";
 import { useState } from "react";
 import "./data/database.json";
+import firebase from "firebase";
+import { database, storage } from './firebase';
 
 const CreatePost = () => {
 
     var Nickname = "Pseudo";
+    const [image, setImage] = useState(null);
     const [description, setDescription] = useState("");
     const [location, setLocation] = useState("");
     var picture;
+
+    const handleChange = e => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    }
+
+    const handleSubmit = () => {
+        const uploadImage = storage.ref(`images/${image.name}`).put(image);
+
+        uploadImage.on(
+            "state_changed",
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                storage
+                    .ref("images")
+                    .child(image.name)
+                    .getDownloadURL()
+                    .then( url => {
+                        database.collection("posts").add({
+                            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                            caption: description,
+                            location: location,
+                            imageUrl: url,
+                            username: "someone"
+                        });
+                        setImage = null;
+                        setDescription = "";
+                        setLocation = "";
+                    });
+            }
+        );
+    };
 
     return (
         <div className="create">
@@ -26,9 +65,9 @@ const CreatePost = () => {
         </table>
         <br/>&nbsp;
         <label for = "file"> Select the image you want to share: </label><br/>&nbsp;
-        <input type="file" id = "image" name="create-image" accept="image/png, image/jpeg, image/jpg" value={picture} required />
+        <input type="file" id = "image" name="create-image" accept="image/png, image/jpeg, image/jpg" value={ image } required onChnage = { handleChange }/>
         <br /> <br />&nbsp;
-        <button type="submit" onClick= {(e) => alert("submitted")} > <h4>Post</h4> </button> 
+        <button type="submit" onClick={ handleSubmit }> <h4>Post</h4> </button> 
         </form> <br/>
         <p><b> &nbsp; &nbsp; {Nickname}</b></p>
         <p class="location"> &nbsp; &nbsp; {location} </p> <br/>
