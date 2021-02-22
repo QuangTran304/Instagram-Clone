79a905b42df260573e9a865413065d0f8602a7ce
Tushar Raval <tushar.raval102@gmail.com>
2021-02-21 20:00:50 -0500

    fixed error after clicking submit button

--- src/SignUp.js
+++ src/SignUp.js
@@ -12,7 +12,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {auth, database, generateUserDocument} from "./firebase";
+import {auth, generateUserDocument} from "./firebase";
 import {useState} from 'react';
 
 function Copyright() {
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyD1q68FCFm9WSoUvnL6FgrOhwEe_RjZ-5k",
@@ -15,7 +15,7 @@ const storage = firebase.storage();
 
 export const generateUserDocument = async (user, additionalData) => {
   if (!user) return;
-  const userRef = firebase.firestore.doc(`users/${user.uid}`);
+  const userRef = database.doc(`users/${user.uid}`);
   const snapshot = await userRef.get();
   if (!snapshot.exists) {
     const { email, firstName, lastName, photoURL, username } = user;
@@ -37,7 +37,7 @@ export const generateUserDocument = async (user, additionalData) => {
 const getUserDocument = async uid => {
   if (!uid) return null;
   try {
-    const userDocument = await firebase.firestore.doc(`users/${uid}`).get();
+    const userDocument = await database.doc(`users/${uid}`).get();
     return {
       uid,
       ...userDocument.data()
