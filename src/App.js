8f51ffbe6c0f97cd887699c5640acff92a6cf1c4
Tushar Raval <tushar.raval102@gmail.com>
2021-03-18 23:37:49 -0400

    #52 Follow feature implemented without bugs
    Co-authored-by: anonna16 <anonna16@users.noreply.github.com>
    Co-authored-by: Nareg Mouradian <naregschoolwork@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Navbar from './component/Navbar'
 import SignIn from "./component/SignIn";
 import SignUp from "./component/SignUp";
 import Follow from "./component/Follow"
-import UnFollow from "./component/Unfollow"
 import {firebaseAuth} from "./provider/AuthProvider"
 import firebase from "firebase"
 
 function App() {
   const {token} = useContext(firebaseAuth)
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={ rProps => token === null ? <SignIn /> : [<Navbar />, <CreatePost />,<Follow />,<UnFollow />, <Post />]}/>
+        <Route exact path="/" render={ rProps => token === null ? <SignIn /> : [<Navbar />, <CreatePost />,<Follow />, <Post />]}/>
         <Route exact path="/signin">
           {rProps => token === null ? <Redirect to='/' /> : [<Navbar />, <CreatePost />, <Post />]}
         </Route>
--- src/component/Unfollow.js
+++ src/component/Unfollow.js
@@ -1,19 +1,14 @@
 import React from "react"
 import { useState, useEffect } from "react"
-import { makeStyles } from '@material-ui/core/styles'
 import firebase from "firebase"
 import { database } from "../firebase/firebase"
-import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import "../index.css"
-import { useRef } from 'react'
-import { Brightness1Rounded } from "@material-ui/icons"
 
 const UnFollow = () => {
     const [following, setFollowing] = useState([]);
-    const [follower, setFollower] = useState([]);
+    // const [follower, setFollower] = useState([]);
     const [users, setUsers] = useState([]);
-    var currentId= "";
 
     useEffect(() => {
         database
@@ -28,66 +23,30 @@ const UnFollow = () => {
     }, [])
 
     useEffect(() => {
-            if(currentId){
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
                 database
-                .collection('users')
-                .doc(currentId)
-                .collection('following')
-                .onSnapshot(snapshot => {
-                    setFollowing(
-                        snapshot.docs.map(doc => ({
-                            username: doc.data().username
-                        })));
-                })
+                    .collection('users')
+                    .doc(firebase.auth().currentUser.displayName)
+                    .collection('following')
+                    .onSnapshot(snapshot => {
+                        setFollowing(
+                            snapshot.docs.map(doc => ({
+                                username: doc.data().username
+                            })));
+                    })
+
+            } else {
+                // No user is signed in.
             }
-            
-    }, [currentId])
-
-    // useEffect(() => {
-    //         if(currentId){
-    //             database
-    //             .collection('users')
-    //             .doc(currentId)
-    //             .collection('following')
-    //             .onSnapshot(snapshot => {
-    //                 setFollowing(
-    //                     snapshot.docs.map(doc => ({
-    //                         username: doc.data().username
-    //                     })));
-    //             })
-    //         }
-    // }, [currentId])
-
-    function findUser() {
-        users.map(({user }) => {
-            if (user === firebase.auth().currentUser.displayName) {
-                currentId = firebase.auth().currentUser.displayName;
-                console.log(user)
-            }
-        })
-     }
-    
-    const unFollowUser = (username) => {
-        database.collection('users').doc(firebase.auth().currentUser.displayName).collection('following').doc(username).delete().then(() => {
-            console.log("User successfully deleted from the following guy!");
-        }).catch((error) => {
-            console.error("Error removing user: ", error);
         });
+    }, [])
 
-        database.collection('users').doc(username).collection('follower').doc(firebase.auth().currentUser.displayName).delete().then(() => {
-            console.log("User successfully deleted from the follower guy!");
-        }).catch((error) => {
-            console.error("Error removing user: ", error);
-        });
-    }
-    
     return (
         <div>
-            {findUser()}
             {following.map(({ username }) => (
                 <div>
-                    {console.log(username + " should be username")}
-                    {username} <Button onClick={ () => unFollowUser({username}) }>Unfollow</Button>
+                    {username} <Button>Unfollow</Button>
                 </div>
             )
             )}
@@ -96,3 +55,18 @@ const UnFollow = () => {
 }
 
 export default UnFollow;
+
+// This is to be used to implement unFollow later: 
+// const unFollowUser = (username) => {
+    //     database.collection('users').doc(firebase.auth().currentUser.displayName).collection('following').doc(username).delete().then(() => {
+    //         console.log("User successfully deleted from the following guy!");
+    //     }).catch((error) => {
+    //         console.error("Error removing user: ", error);
+    //     });
+
+    //     database.collection('users').doc(username).collection('follower').doc(firebase.auth().currentUser.displayName).delete().then(() => {
+    //         console.log("User successfully deleted from the follower guy!");
+    //     }).catch((error) => {
+    //         console.error("Error removing user: ", error);
+    //     });
+    // }
