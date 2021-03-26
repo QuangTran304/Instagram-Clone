e3e24d9a05c79408e6e863f46142e5305e721f32
Quang <nhatquang304@gmail.com>
2021-03-26 02:51:43 -0400

    #54 first test with popup post modal onClick

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -4,7 +4,8 @@ import { database } from '../firebase/firebase';
 import firebase from 'firebase'
 import "../index.css"
 import Button from '@material-ui/core/Button';
-
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 const Profile = () => {  
@@ -12,7 +13,34 @@ const Profile = () => {
   const [following, setFollowing] = useState();
   const [postCount, setPostCount] = useState();
   const [userPosts, setUserPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
+  const [currentPost, setCurrentPost] = useState([]);
+
+  const handleOpen = (post) => {
+    setOpen(true);
+    // setCurrentPost(post);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // function rand() {
+  //   return Math.round(Math.random() * 20) - 10;
+  // }
+
+  // function getModalStyle() {
+  //   const top = 50 + rand();
+  //   const left = 50 + rand();
+  
+  //   return {
+  //     top: `${top}%`,
+  //     left: `${left}%`,
+  //     transform: `translate(-${top}%, -${left}%)`,
+  //   };
+  // }
+  // const [modalStyle] = useState(getModalStyle);
 
   useEffect(() => {
     database
@@ -86,12 +114,18 @@ const Profile = () => {
     </div>
 
 
+
     <div className="profile-userPosts">
     {
       userPosts.map( ({id, post}) => (
-        <img src={post.image} alt={id}/>
+        <img src={post.image} alt={id} onClick={ () => handleOpen(post) }/>
       ))
     }
+
+      <Modal open={open} onClose={handleClose} >
+        {/* <img src={currentPost.image} alt="" /> */}
+        <img src="https://source.unsplash.com/random/" style={{width: '80%'}} alt="" />
+      </Modal>
     </div>
 
   </>
