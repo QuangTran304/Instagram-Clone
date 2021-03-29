37c1b697bf919f0e6faa1116db7cbbac347ed709
Tushar Raval <tushar.raval102@gmail.com>
2021-03-28 23:10:30 -0400

    #62 adding new import to react

--- src/component/Post.js
+++ src/component/Post.js
@@ -5,6 +5,7 @@ import ChatBubbleOutlineOutlinedIcon from "@material-ui/icons/ChatBubbleOutlineO
 import { database } from "../firebase/firebase";
 import Comment from "./Comment";
 import Like from "./Like";
+import Popup from 'reactjs-popup'
 
 const Post = () => {
   const [posts, setPosts] = useState([]);
@@ -17,12 +18,37 @@ const Post = () => {
         setPosts(
           snapshot.docs.map((doc) => ({
             id: doc.id,
-            post: doc.data(),
+            post: doc.data()
           }))
-        );
+        )
       });
     // eslint-disable-next-line
   }, []);
+
+  // function PopUpLike(id) {
+  //   const [liked, setLiked] = useState([]);
+
+  //   if(id){
+  //     database
+  //     .collection("posts")
+  //     .doc(id)
+  //     .collection('likes')
+  //     .orderBy('timestamp', 'asc')
+  //     .onSnapshot((snapshot) => {
+  //         setLiked(
+  //           snapshot.docs.map((doc) => ({
+  //             username: doc.data().username,
+  //           }))
+  //         )
+  //     })
+  //   }
+
+  //     return liked.map(({username}) => (
+  //       <div>
+  //         {username}
+  //       </div>
+  //     ))
+  // }
 
   return posts.map(({ id, post }) => (
     <div className="post" key={id}>
@@ -48,8 +74,12 @@ const Post = () => {
             style={{ marginRight: 8, width: 20 }}
           />
         </div>
+        <p className="post-like-number"> Liked by {post.likes} 
+        <Popup trigger={<span> people </span>} modal>
+          {/* <span> <PopUpLike id = {id} /> </span> */}
+        </Popup>
+        </p>
 
-        <p className="post-like-number"> Liked by {post.likes} people</p>
 
         <h4 className="post-description">
           <strong>{post.username}</strong> {post.description}
